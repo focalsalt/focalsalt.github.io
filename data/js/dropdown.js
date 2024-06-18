6a967ce6d8db7dc46fb3c3b0c10cdb2c5a5b5c92
Focalsalt <68410113+focalsalt@users.noreply.github.com>
2024-06-18 12:57:02 +0800

    I dont know
    Changes to be committed:
    	modified:   data/js/dropdown.js

--- data/js/dropdown.js
+++ data/js/dropdown.js
@@ -6,8 +6,8 @@ function dropdown() {
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (e) {
-	if (!e.target.matches('.dropbtn')) {
-		var myDropdown = document.getElementById('myDropdown');
+	if (!e.target.matches('.dropdown-button')) {
+		var myDropdown = document.getElementById('Dropdown');
 		if (myDropdown.classList.contains('show')) {
 			myDropdown.classList.remove('show');
 		}
