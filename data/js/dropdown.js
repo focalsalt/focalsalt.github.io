d087eae9e7589a477270735aca640ea5a5dc4ccf
Focalsalt <68410113+focalsalt@users.noreply.github.com>
2024-06-18 12:43:31 +0800

    Fix Bug
    Changes to be committed:
    	modified:   data/js/dropdown.js
    	modified:   data/template/navbar.html
    	modified:   index.html
    	modified:   pages/datapack/focal-backpack.html
    	modified:   pages/resourcepack/focal-common.html
    	modified:   pages/resourcepack/undertale-ui.html
    	modified:   pages/server/voidland.html
    	modified:   pages/server/workshop.html

--- data/js/dropdown.js
+++ data/js/dropdown.js
@@ -1,15 +1,24 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function dropdown() {
-    document.getElementById('Dropdown').classList.toggle('show');
+function dropdown1() {
+	document.getElementById('Dropdown1').classList.toggle('show');
+}
+
+function dropdown2() {
+	document.getElementById('Dropdown2').classList.toggle('show');
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (e) {
-    if (!e.target.matches('.dropbtn')) {
-        var myDropdown = document.getElementById('myDropdown');
-        if (myDropdown.classList.contains('show')) {
-            myDropdown.classList.remove('show');
-        }
-    }
+	if (!e.target.matches('.dropbtn')) {
+		var myDropdown = document.getElementById('Dropdown1');
+		if (myDropdown.classList.contains('show')) {
+			myDropdown.classList.remove('show');
+		}
+
+		var myDropdown = document.getElementById('Dropdown2');
+		if (myDropdown.classList.contains('show')) {
+			myDropdown.classList.remove('show');
+		}
+	}
 };
