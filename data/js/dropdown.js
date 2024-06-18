4956944b7b1960ebee31faf6acf0dd08ecc3e75c
Focalsalt <68410113+focalsalt@users.noreply.github.com>
2024-06-18 12:46:58 +0800

    Fix again
    Changes to be committed:
    	modified:   data/js/dropdown.js
    	modified:   data/template/navbar.html
    	modified:   index.html
    	modified:   pages/datapack/focal-backpack.html
    	modified:   pages/resourcepack/focal-common.html
    	modified:   pages/resourcepack/undertale-ui.html
    	modified:   pages/server/voidland.html
    	modified:   pages/server/workshop.html

--- data/js/dropdown.js
+++ data/js/dropdown.js
@@ -1,22 +1,13 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function dropdown1() {
-	document.getElementById('Dropdown1').classList.toggle('show');
-}
-
-function dropdown2() {
-	document.getElementById('Dropdown2').classList.toggle('show');
+	document.getElementById('Dropdown').classList.toggle('show');
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (e) {
 	if (!e.target.matches('.dropbtn')) {
-		var myDropdown = document.getElementById('Dropdown1');
-		if (myDropdown.classList.contains('show')) {
-			myDropdown.classList.remove('show');
-		}
-
-		var myDropdown = document.getElementById('Dropdown2');
+		var myDropdown = document.getElementById('Dropdown');
 		if (myDropdown.classList.contains('show')) {
 			myDropdown.classList.remove('show');
 		}
